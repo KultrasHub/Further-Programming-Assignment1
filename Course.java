3a42c368eb71eca62fa2c717dba3f868fa4a6dba
Khoa <s3863956@rmit.edu.vn>
2022-03-28 21:54:11 +0700

    Add Sem to  courses

--- Course.java
+++ Course.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 
 public class Course {
     private String id;
     private String name;
     private int numberOfCredit;
+    private ArrayList<String> semList;
     //Course 
     public Course(String id,String name,int credit)
     {
@@ -17,6 +17,35 @@ public class Course {
         this.id=id;
         this.name=name;
         numberOfCredit=0;
+    }
+    //Builder to add semester
+    public Course addSemester(String sem)
+    {
+        //check if sem exist in semList
+        for(int i=0;i<semList.size();i++)
+        {
+            if(sem==semList.get(i))
+            {
+                //the sem already exits inn semList
+                return this;
+            }
+        }
+        semList.add(sem);
+        return this;
+    }
+    //Check if course contains a sem
+    public boolean checkSem(String sem)
+    {
+         //check if sem exist in semList
+         for(int i=0;i<semList.size();i++)
+         {
+             if(sem==semList.get(i))
+             {
+                 //the sem already exits inn semList
+                 return true;
+             }
+         }
+         return false;
     }
     //private ArrayList<Student> students=new ArrayList<Student>();
     public String getId()
